890044de318af3a154250cf35d7ad102b3a71bb8
Alex Pepi <apepi@sta-09004494.bordeaux.inria.fr>
2025-12-16 11:23:56 +0100

    Fixes about imports

--- rosoku/__init__.py
+++ rosoku/__init__.py
@@ -1,11 +1,2 @@
 __version__ = "0.0.3"
-
-from .conventional import *
-from .deeplearning import *
-
-from .alias import *
-
-from . import preprocessing
-from . import utils
-from . import tl
-from . import attribution
+__all__ = ["attribution", "preprocessing", "tl", "utils", "alias", "conventional", "deeplearning"]
